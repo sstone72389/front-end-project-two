647296a062eba97d73f223b9387c048901f08e15
“sstone72389” <sstone72389@gmail.com>
2017-05-03 09:49:47 -0400

    Completes html/css responsive design
    adds additional media queries to ensure responsive design. Using
    chrome developer tools, app has been tested on iphones and galaxy phones

--- assets/scripts/checklist.js
+++ assets/scripts/checklist.js
@@ -1,52 +1,44 @@
-// startList to be exported and linked
-const startList = function () {
-  const incompleteTasks = []
-  const completeTasks = []
-  // fucntion that adds input field to ordered list when clicked
-  $(document).ready(
-    function () {
-      $('#button').click(
-        function () {
-          const toAdd = $('.listInput').val()
-          $('ol').append('<li>' + toAdd + '</li>')
-          incompleteTasks.push(toAdd)
-          $('.listInput').val('')
-          console.log(incompleteTasks)
-        })
-      // allows you to hit enter to add items
-      $('.listInput').keypress(function (event) {
-        if (event.which === 13) {
-          $('#button').click()
-          event.preventDefault()
-        }
-      })
-
-      // strikes and fades out clicked list items
-      // adds value of clicked item into completeTasks array
-      $(document).on('click', 'li', function () {
-        $(this).toggleClass('strike').fadeOut('slow')
-        // checks index of list item, may ne unimportant
-        // const index = $(this).index()
-        const text = $(this).text()
-        completeTasks.push(text)
-        console.log(completeTasks)
-      })
-
-      $('input').focus(function () {
-        $(this).val('')
-      })
-    }
-  )
-}
-
-const addedItem = function () {
-  $('#button').on('click', function () {
-    $('.UAtext').text('Are you forgetting something?')
-    $('.UAtext').show
-  })
-}
-
-module.exports = {
-  startList,
-  addedItem
-}
+// // startList to be exported and linked
+// const startList = function () {
+//   const incompleteTasks = []
+//   const completeTasks = []
+//   // fucntion that adds input field to ordered list when clicked
+//   $(document).ready(
+//     function () {
+//       $('#button').click(
+//         function () {
+//           const toAdd = $('.listInput').val()
+//           $('ol').append('<li>' + toAdd + '</li>')
+//           incompleteTasks.push(toAdd)
+//           $('.listInput').val('')
+//           console.log(incompleteTasks)
+//         })
+//       // allows you to hit enter to add items
+//       $('.listInput').keypress(function (event) {
+//         if (event.which === 13) {
+//           $('#button').click()
+//           event.preventDefault()
+//         }
+//       })
+//
+//       // strikes and fades out clicked list items
+//       // adds value of clicked item into completeTasks array
+//       $(document).on('click', 'li', function () {
+//         $(this).toggleClass('strike').fadeOut('slow')
+//         // checks index of list item, may ne unimportant
+//         // const index = $(this).index()
+//         const text = $(this).text()
+//         completeTasks.push(text)
+//         console.log(completeTasks)
+//       })
+//
+//       $('input').focus(function () {
+//         $(this).val('')
+//       })
+//     }
+//   )
+// }
+//
+// module.exports = {
+//   startList
+// }
